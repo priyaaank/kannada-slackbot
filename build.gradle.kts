f13370a4ce93418843ed4a31948706c0be917b0d
Priyank Gupta <priyaaank@gmail.com>
2024-03-08 15:45:37 +0530

    Setting up initial kotlin jetty repo with a url and a gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.9"
+}
+
+group = "me.priyaaank"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("me.priyaaank.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+    jvmToolchain(8)
+}
---
+++ src/main/kotlin/me/priyaaank/Application.kt
@@ -0,0 +1,16 @@
+package me.priyaaank
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import me.priyaaank.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+@Suppress("unused")
+fun Application.module() {
+    configureRouting()
+}
